0fbebed10291d1dc277e144f6993aa1cd8ad5020
Kelly Steensma <kellysteensma@gmail.com>
2013-05-13 09:56:17 -0500

    All prompter classes are complete.

--- test/prompter_test.erl
+++ test/prompter_test.erl
@@ -1,83 +1,100 @@
 -module(prompter_test).
 -include_lib("eunit/include/eunit.hrl").
 
-guess_1_test() ->
+guess_1_with_one_valid_guess_test() ->
           meck:new(gets),
           meck:expect(gets, response, fun() -> "aaaa" end),
           ?assertEqual(prompter:guess(12, ["a","b","c","d","e","f"]), ["a","a","a","a"]),
-          ?assert(meck:validate(gets)),
           meck:unload(gets).
 
+guess_2_with_one_guess_of_3_valid_symbols_test() ->
+          meck:new(gets),
+          meck:expect(gets, response, fun() -> "aaa" end),
+          meck:expect(gets, response, fun() -> "aaaa" end),
+          ?assertEqual(prompter:guess(12, ["a","b","c","d","e","f"]), ["a","a","a","a"]),
+          meck:unload(gets).
+
+guess_3_with_one_guess_of_1_invalid_symbols_test() ->
+          meck:new(gets),
+          meck:expect(gets, response, fun() -> "." end),
+          meck:expect(gets, response, fun() -> "aaaa" end),
+          ?assertEqual(prompter:guess(12, ["a","b","c","d","e","f"]), ["a","a","a","a"]),
+          meck:unload(gets).
+
+guess_4_with_one_guess_of_1_space_test() ->
+          meck:new(gets),
+          meck:expect(gets, response, fun() -> " " end),
+          meck:expect(gets, response, fun() -> "aaaa" end),
+          ?assertEqual(prompter:guess(12, ["a","b","c","d","e","f"]), ["a","a","a","a"]),
+          meck:unload(gets).
+
+guess_5_with_one_guess_of_4_invalid_symbols_test() ->
+          meck:new(gets),
+          meck:expect(gets, response, fun() -> "gggg" end),
+          meck:expect(gets, response, fun() -> "aaaa" end),
+          ?assertEqual(prompter:guess(12, ["a","b","c","d","e","f"]), ["a","a","a","a"]),
+          meck:unload(gets).
+
+guess_6_with_one_guess_of_4_invalid_numbers_test() ->
+          meck:new(gets),
+          meck:expect(gets, response, fun() -> "1234" end),
+          meck:expect(gets, response, fun() -> "aaaa" end),
+          ?assertEqual(prompter:guess(12, ["a","b","c","d","e","f"]), ["a","a","a","a"]),
+          meck:unload(gets).
+
+guess_7_with_one_guess_of_4_capitalized_symbols_test() ->
+          meck:new(gets),
+          meck:expect(gets, response, fun() -> "BBBB" end),
+          meck:expect(gets, response, fun() -> "aaaa" end),
+          ?assertEqual(prompter:guess(12, ["a","b","c","d","e","f"]), ["a","a","a","a"]),
+          meck:unload(gets).
 
 % guess_1_test() ->
-%   [{ "displays an invalid selection message, prompts the player for a guess, returns the guess in a list datatype and validates the guess",
-%       ?assertEqual(prompter:guess("aaaa", 12, ["a","b","c","d","e","f"]), console_io:display_invalid_selection())
-%       ?assertEqual(prompter:guess("aaaa", 12, ["a","b","c","d","e","f"]), prompter:prompt())
-%       ?assertEqual(prompter:guess("aaaa", 12, ["a","b","c","d","e","f"]), ["a","a","a","a"])
-%       ?assertEqual(prompter:guess("aaaa", 12, ["a","b","c","d","e","f"]), prompter:validate_guess(["a","a","a","a"], 12, ["a","b","c","d","e","f"]))
+%   [{ "displays a 'prompt for guess' message, an invalid selection message when guess is invalid",
+%       ?assertEqual(1,1)
 %   }].
 
-% validate_guess_1_test() ->
-%   [{ "prompts for a new guess when the guess is only 3 characters and should be 4",
-%       ?assertEqual(prompter:validate_guess(["g","g","g"], 12, ["a","b","c","d","e","f"]), prompter:guess(12, ["a","b","c","d","e","f"]))
-%   }].
+play_again_1_with_a_response_of_y_test() ->
+          meck:new(gets),
+          meck:expect(gets, response, fun() -> "y" end),
+          ?assertEqual(prompter:play_again(), "y"),
+          meck:unload(gets).
 
-% validate_guess_2_test() ->
-%   [{ "prompts for a new guess when the guess is not 4 valid symbols",
-%       ?assertEqual(prompter:validate_guess(["."], 12, ["a","b","c","d","e","f"]), prompter:guess(12, ["a","b","c","d","e","f"]))
-%   }].
+play_again_2_with_a_response_of_n_test() ->
+          meck:new(gets),
+          meck:expect(gets, response, fun() -> "n" end),
+          ?assertEqual(prompter:play_again(), "n"),
+          meck:unload(gets).
 
-% validate_guess_3_test() ->
-%   [{ "prompts for a new guess when the guess is not 4 valid symbols",
-%       ?assertEqual(prompter:validate_guess([" ", " "], 12, ["a","b","c","d","e","f"]), prompter:guess(12, ["a","b","c","d","e","f"]))
-%   }].
+play_again_3_with_an_invalid_response_of_space_test() ->
+          meck:new(gets),
+          meck:expect(gets, response, fun() -> " " end),
+          meck:expect(gets, response, fun() -> "y" end),
+          ?assertEqual(prompter:play_again(), "y"),
+          meck:unload(gets).
 
-% validate_guess_4_test() ->
-%   [{ "prompts for a new guess when the guess is not 4 valid symbols",
-%       ?assertEqual(prompter:validate_guess(["g","g","g","g"], 12, ["a","b","c","d","e","f"]), prompter:guess(12, ["a","b","c","d","e","f"]))
-%   }].
+play_again_4_with_an_invalid_response_of_a_period_test() ->
+          meck:new(gets),
+          meck:expect(gets, response, fun() -> "." end),
+          meck:expect(gets, response, fun() -> "y" end),
+          ?assertEqual(prompter:play_again(), "y"),
+          meck:unload(gets).
 
-% validate_guess_5_test() ->
-%   [{ "prompts for a new guess when the guess is not 4 valid symbols",
-%       ?assertEqual(prompter:validate_guess(["1","2","2","3"], 12, ["a","b","c","d","e","f"]), prompter:guess(12, ["a","b","c","d","e","f"]))
-%   }].
+play_again_3_with_an_invalid_response_of_an_integer_test() ->
+          meck:new(gets),
+          meck:expect(gets, response, fun() -> "3" end),
+          meck:expect(gets, response, fun() -> "y" end),
+          ?assertEqual(prompter:play_again(), "y"),
+          meck:unload(gets).
 
-validate_guess_6_test() ->
-  [{ "returns the guess in a list datatype when the guess is valid",
-      ?assertEqual(prompter:validate_guess(["e","f","e","f"], 12, ["a","b","c","d","e","f"]), ["e","f","e","f"])
-  }].
+play_again_3_with_an_invalid_response_of_a_capitalized_y_test() ->
+          meck:new(gets),
+          meck:expect(gets, response, fun() -> "Y" end),
+          meck:expect(gets, response, fun() -> "y" end),
+          ?assertEqual(prompter:play_again(), "y"),
+          meck:unload(gets).
 
 % play_again_1_test() ->
-%   [{ "prompts the player if they want to play again and validates the response",
-%       ?assertEqual(prompter:play_again(), prompter:prompt())
-%       ?assertEqual(prompter:play_again(), prompter:validate_play_again("y"))
+%   [{ "displays a 'prompt for play again' message, an invalid selection message when the response is invalid",
+%       ?assertEqual(1,1)
 %   }].
-
-% validate_play_again_1_test() ->
-%   [{ "displays an invalid selection message and prompts the player for a new play again response when the response is invalid",
-%       ?assertEqual(prompter:validate_play_again("."), console_io:display_invalid_selection())
-%       ?assertEqual(prompter:validate_play_again("."), prompter:prompt())
-%   }].
-
-% validate_play_again_2_test() ->
-%   [{ "displays an invalid selection message and prompts the player for a new play again response when the response is invalid",
-%       ?assertEqual(prompter:validate_play_again(" "), console_io:display_invalid_selection())
-%       ?assertEqual(prompter:validate_play_again(" "), prompter:prompt())
-%   }].
-
-% validate_play_again_3_test() ->
-%   [{ "displays an invalid selection message and prompts the player for a new play again response when the response is invalid",
-%       ?assertEqual(prompter:validate_play_again("Y"), console_io:display_invalid_selection())
-%       ?assertEqual(prompter:validate_play_again("Y"), prompter:prompt())
-%   }].
-
-% validate_play_again_4_test() ->
-%   [{ "displays an invalid selection message and prompts the player for a new play again response when the response is invalid",
-%       ?assertEqual(prompter:validate_play_again("yy"), console_io:display_invalid_selection())
-%       ?assertEqual(prompter:validate_play_again("yy"), prompter:prompt())
-%   }].
-
-validate_play_again_5_test() ->
-  [{ "returns the play again response when the response is valid",
-      ?assertEqual(prompter:validate_play_again("y"), "y")
-  }].
